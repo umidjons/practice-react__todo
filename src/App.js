6820899f9e909f47e257cf4737f5973121620f34
umidjons <almatov.us@gmail.com>
2019-03-23 11:25:46 +0500

    Installed bootstrap. Created todo list header.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: 'Adam'
+    };
+  }
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === 'Adam' ? 'Bob' : 'Adam'
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h4 className="bg-primary text-white text-center p-2">{this.state.userName}'s To Do List</h4>
+        <button className="btn btn-primary m-2" onClick={this.changeStateData}>Change</button>
       </div>
     );
   }
 }
-
-export default App;
